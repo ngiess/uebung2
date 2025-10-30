403823410ef9a404195fee6549c6c0a11a40e40a
Nico Gieß <ngiess@posteo.de>
2025-10-30 15:17:22 +0100

    Aufg1c

---
+++ src/p3/P3_main.java
@@ -0,0 +1,16 @@
+package p3;
+
+public class P3_main {
+	public static void main(String[] args) {
+		int i = 10, j = 55, k = -7;
+		if (i < j) {
+			System.out.println("A");
+		}
+		if (i < k) {
+			System.out.println("B");
+		}
+		if (j < k) {
+			System.out.println("C");
+		}
+	}
+}
